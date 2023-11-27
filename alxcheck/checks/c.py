0fb64092f1932bb9580155fb5add7722b51925eb
be-great <begreat5333@gmail.com>
2023-11-27 06:30:14 +0200

    adjust c.py

--- alxcheck/checks/c.py
+++ alxcheck/checks/c.py
@@ -1,11 +1,20 @@
 import subprocess
 from ..utils.error_logging import print_please_install_betty
 
-
 def betty_check(file_path):
     try:
         result = subprocess.run(["betty", file_path])
     except Exception:
         print_please_install_betty()
         return False
+    # because it have different ouput meaning looking for errors
+    try:
+        result1 = subprocess.run(["betty", file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    except subprocess.CalledProcessError as e:
+        return False
+
+    if "ERROR:" in result1.stdout or "ERROR:" in result1.stderr:
+        return False
+
     return result.returncode == 0
+
